0a158727c992785a0a2e68ac0ec99296bd1af977
adas6691 <114636625+adas6691@users.noreply.github.com>
2022-09-29 11:22:32 +0530

    Add files via upload

---
+++ Code_Java.java
@@ -0,0 +1,8 @@
+public class Code_Java {
+    public static void main(String[] args) {
+        //output
+        System.out.print("Hello World with Java\n");
+        System.out.print("Hello World with Java\n");
+        System.out.print("Hello World with Java");
+    }
+}
